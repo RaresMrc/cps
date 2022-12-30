2bffab0e6d3aa5fcd7920574c059faf1ce10c796
RaresMrc <rcm14052004@gmail.com>
2022-12-30 19:56:38 +0200

    Fixed delivery of static files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use('/script', express.static(__dirname + '/scripts/main.js'));
+app.use('/script', express.static(__dirname + '/scripts'));
 app.use('/style', express.static(__dirname + '/style.css'));
 
 app.get('/', (req, res) => {
